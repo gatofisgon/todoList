d0d8d3a777123caefa52470fcda2e5c72723ab1c
Kevin <kevinsaezramirez@gmail.com>
2022-03-01 22:10:18 -0300

    BETA Version - W/O POST

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+
+    res.render('index', {foo: 'FOO'});
+
+    var today = new Date();
+
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    var day = today.toLocaleDateString("en-US", options);
+
+    res.render("list", {kindOfDay: day});
+
+});
+
+
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000");
+});
