97da6621ac3661a2eaa0fe0f70e71d8ba192ab3a
Kevin <kevinsaezramirez@gmail.com>
2022-03-02 19:53:56 -0300

    Final Version - True Edition (I hope so)

--- app.js
+++ app.js
@@ -35,6 +35,10 @@ app.post("/", function(req, res){
     res.redirect("/")
 });
 
+app.get("/about", function(req, res){
+    res.render("about");
+})
+
 
 app.listen(3000, function(){
     console.log("Server started on port 3000");
